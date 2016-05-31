c7e369911cb6c1cda95f4ddb58aa45238b43f29e
汤文洪 <Tang@MacdeiMac.local>
2016-05-31 17:28:09 +0800

    Initial Commit

---
+++ OC__myGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC__myGit
+//
+//  Created by 汤文洪 on 16/5/31.
+//  Copyright © 2016年 Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OC__myGit/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OC__myGit
+//
+//  Created by 汤文洪 on 16/5/31.
+//  Copyright © 2016年 Tang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
